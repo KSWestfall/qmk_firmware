9bb2cd4af48226a6a02cb7f04f7aaa914a9f5595
Kyle Westfall <Kyle.Westfall@pressganey.com>
2020-04-16 12:39:19 -0400

    6x6 manuform LED setup

---
+++ keyboards/handwired/dactyl_manuform/6x6/keymaps/twd/config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define USE_SERIAL
+
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+//#define EE_HANDS
+// Rows are doubled-up
+
+#define RGBLED_SPLIT { 6, 6 }
+#define RGBLIGHT_ANIMATIONS
